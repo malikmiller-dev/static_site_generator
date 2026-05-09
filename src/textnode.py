c5721e614af418376ca0a4410cbf3112cb9bf306
malikmillerdev <malikmillerbusiness96@gmail.com>
2026-05-09 15:37:06 -0400

    pushing content

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 from htmlnode import LeafNode
 
@@ -62,6 +63,9 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.LINK:
         return LeafNode('a', text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
-        return LeafNode('img', '', {"src": text_node.url, "alt": text_node.text})
+        src = text_node.url
+        if src.startswith('/'):
+            src = f'/images/{os.path.basename(src)}'
+        return LeafNode('img', '', {'src': src, 'alt': text_node.text})
 
     raise Exception('Text Type not found')
