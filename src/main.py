1b5619ec263800841a321f50eda6ad28d3d6e817
malikmillerdev <malikmillerbusiness96@gmail.com>
2026-05-09 15:25:14 -0400

    pushing content

--- src/main.py
+++ src/main.py
@@ -26,6 +26,7 @@ def main():
         basepath = sys.argv[1]
     else:
         basepath = '/'
+
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
     static_path = os.path.join(project_root, "static")
@@ -103,9 +104,8 @@ def generate_page(from_path, template_path, dest_path, new_name, basepath):
 
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     for items in os.listdir(dir_path_content):
-        content_path = os.path.join(project_root, os.path.join(dir_path_content, items))
+        content_path = os.path.join(dir_path_content, items)
         if os.path.isdir(content_path):
             generate_pages_recursive(content_path, template_path, dest_dir_path, basepath)
         if items.endswith('.md'):
